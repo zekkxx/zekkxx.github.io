fea635f7447e0959ecfe290b0643d7a85148903f
Kieran 'zekkxx' Anthony <zekkxx@gmail.com>
2020-03-25 15:24:09 -0500

    Rendering projects dynamically from site.js implemented

--- assets/javascript/site.js
+++ assets/javascript/site.js
@@ -1,24 +1,42 @@
-function adjustTray(){
-    $(".nav-list a").toggle("slow");
+var projects = [
+    {"name": "Word Guessing Game", "img": "WordGuessGame", "link": "https://zekkxx.github.io/word-guess-game/"},
+    {"name": "Memory Game", "img": "memory_game", "link": "https://shielded-ridge-15296.herokuapp.com/"},
+    {"name": "Trivia Game", "img": "TriviaGame", "link": "https://zekkxx.github.io/trivia-game/"},
+    {"name": "Basic Game", "img": "StarWarsGame", "link": "https://zekkxx.github.io/basic-game/"},
+    {"name": "LIRI", "img": "liri", "link": "https://github.com/zekkxx/liri"},
+    {"name": "Bamazon", "img": "bamazon", "link": "https://github.com/zekkxx/bamazon"},
+    {"name": "Soccer Fan Central", "img": "SoccerFanCentral1", "link": "https://iwijetunge.github.io/project1/"},
+    {"name": "Giddy App", "img": "giddy_app", "link": "https://cryptic-tor-78469.herokuapp.com/"},
+    {"name": "Prota", "img": "prota", "link": "prota.herokuapp.com"},
+    {"name": "NYT Search App", "img": "nyt-search", "link": "https://zekkxx.github.io/new-york-times-app/"}
+];
+
+function loadProjectsToContent(){
+    var projectContent = document.getElementById("projectContent");
+    for(let i=0; i<projects.length;i++){
+        var projectContainer = document.createElement("div");
+        projectContainer.setAttribute("class", "imgContainer");
+
+        var projectLink = document.createElement("a");
+        projectLink.setAttribute("target", "_blank");
+        projectLink.setAttribute("href", projects[i].link);
+
+        var projectImg = document.createElement("img");
+        projectImg.setAttribute("src", "assets/images/projects/"
+            + projects[i].img + ".png");
+        projectImg.setAttribute("alt", projects[i].name);
+
+        var projectSpan = document.createElement("span");
+        projectSpan.setAttribute("class", "imgText");
+        projectSpan.textContent = projects[i].name;
+
+        projectLink.append(projectImg);
+        projectLink.append(projectSpan);
+
+        projectContainer.append(projectLink);
+
+        projectContent.append(projectContainer);
+    }
 }
 
-function changeTrayStatus(){
-    if($(".nav-button").attr("value") == "open"){
-        $(".nav-button").attr("value", "closed");
-    } else if($(".nav-button").attr("value") == "closed"){
-        $(".nav-button").attr("value", "open");
-    }
-    adjustTray();
-}
-
-function keepOptionsVisible(){
-    if($(".nav-button").attr("value") == "closed"){
-        $(".nav-button").attr("value", "open");
-        adjustTray();
-    }
-}
-//Currently working on making sure that navbar is always visible
-$(function(){
-    $(".nav-button").click(changeTrayStatus);
-    //$(window).resize(keepOptionsVisible);
-});
+window.onload = loadProjectsToContent;
