91c8bcfd43339aab9afaaac0e5f10ce7d59321d2
Kieran 'zekkxx' Anthony <zekkxx@gmail.com>
2021-01-01 17:48:01 -0600

    Updated js to remove excess html regarding projects

--- assets/javascript/site.js
+++ assets/javascript/site.js
@@ -1,14 +1,13 @@
 var projects = [
-    {"name": "Word Guessing Game", "img": "word_guess_game", "link": "https://zekkxx.github.io/word-guess-game/"},
-    {"name": "Memory Game", "img": "memory_game", "link": "https://shielded-ridge-15296.herokuapp.com/"},
-    {"name": "Trivia Game", "img": "trivia_game", "link": "https://zekkxx.github.io/trivia-game/"},
-    {"name": "Basic Game", "img": "star_wars_game", "link": "https://zekkxx.github.io/basic-game/"},
-    {"name": "LIRI", "img": "liri", "link": "https://github.com/zekkxx/liri"},
-    {"name": "Bamazon", "img": "bamazon", "link": "https://github.com/zekkxx/bamazon"},
-    {"name": "Soccer Fan Central", "img": "soccer_fan_central", "link": "https://iwijetunge.github.io/project1/"},
-    {"name": "Giddy App", "img": "giddy_app", "link": "https://cryptic-tor-78469.herokuapp.com/"},
-    {"name": "Prota", "img": "prota", "link": "prota.herokuapp.com"},
-    {"name": "NYT Search App", "img": "nyt_search", "link": "https://zekkxx.github.io/new-york-times-app/"}
+    {"name": "Word Guessing Game", "img": "word_guess_game", "link": "https://zekkxx.github.io/word-guess-game/", "repo":"https://github.com/zekkxx/word-guess-game"},
+    {"name": "Memory Game", "img": "memory_game", "link": "https://shielded-ridge-15296.herokuapp.com/", "repo":"https://github.com/zekkxx/memory-game"},
+    {"name": "Trivia Game", "img": "trivia_game", "link": "https://zekkxx.github.io/trivia-game/", "repo":"https://github.com/zekkxx/trivia-game"},
+    {"name": "Basic Game", "img": "star_wars_game", "link": "https://zekkxx.github.io/basic-game/", "repo":"https://github.com/zekkxx/basic-game"},
+    {"name": "LIRI", "img": "liri", "link": "https://github.com/zekkxx/liri", "repo":"https://github.com/zekkxx/liri"},
+    {"name": "Bamazon", "img": "bamazon", "link": "https://github.com/zekkxx/bamazon", "repo":"https://github.com/zekkxx/bamazon"},
+    {"name": "Giddy App", "img": "giddy_app", "link": "https://giddyapp.herokuapp.com/", "repo":"https://github.com/zekkxx/giddy-app"},
+    {"name": "Prota", "img": "prota", "link": "prota.herokuapp.com", "repo":"https://github.com/zekkxx/prota"},
+    {"name": "NYT Search App", "img": "nyt_search", "link": "https://zekkxx.github.io/new-york-times-app/", "repo":"https://github.com/zekkxx/new-york-times-app"}
 ];
 
 function applySmoothScrolling(){
@@ -19,37 +18,56 @@ function applySmoothScrolling(){
     });
 }
 
-function loadProjectsToContent(){
-    var projectContent = document.getElementById("projectContent");
-    for(let i=0; i<projects.length;i++){
-        var projectContainer = document.createElement("div");
-        projectContainer.setAttribute("class", "imgContainer");
+function loadProjectSection(){
+    var projectSection = document.createElement("section");
+    var title = document.createElement("h1");
+    title.textContent="Projects"
+    var description = document.createElement("p");
+    description.textContent= "A few of these projects are fully back-end applications, clicking on one of these projects will bring you to the Github repository for the project."
+        +"Additionally, due to the constraints of my wallet, the projects hosted by the free tier of Heroku may have a higher than average loading time when selected.";
+    var breakBar = document.createElement("div");
+    breakBar.setAttribute("class", "breakBar");
+    var projectContent = document.createElement("div");
+    projectContent.setAttribute("class", "projectContent")
+    projects.forEach(project => {
+        var projectContainer = createProjectContainer(project);
+        projectContent.appendChild(projectContainer);
+    });
 
-        var projectLink = document.createElement("a");
-        projectLink.setAttribute("target", "_blank");
-        projectLink.setAttribute("href", projects[i].link);
+    projectSection.appendChild(title);
+    projectSection.appendChild(description);
+    projectSection.appendChild(breakBar);
+    projectSection.appendChild(projectContent)
+    document.getElementById("main").appendChild(projectSection);
+}
 
-        var projectImg = document.createElement("img");
-        projectImg.setAttribute("src", "assets/images/projects/"
-            + projects[i].img + ".png");
-        projectImg.setAttribute("alt", projects[i].name);
+function createProjectContainer(project){
+    var projectContainer = document.createElement("div");
+    projectContainer.setAttribute("class", "imgContainer");
 
-        var projectSpan = document.createElement("span");
-        projectSpan.setAttribute("class", "imgText");
-        projectSpan.textContent = projects[i].name;
+    var projectLink = document.createElement("a");
+    projectLink.setAttribute("target", "_blank");
+    projectLink.setAttribute("href", project.link);
 
-        projectLink.append(projectImg);
-        projectLink.append(projectSpan);
+    var projectImg = document.createElement("img");
+    projectImg.setAttribute("src", "assets/images/projects/"
+        + project.img + ".png");
+    projectImg.setAttribute("alt", project.name);
 
-        projectContainer.append(projectLink);
+    var projectSpan = document.createElement("span");
+    projectSpan.setAttribute("class", "imgText");
+    projectSpan.textContent = project.name;
 
-        projectContent.append(projectContainer);
-    }
+    projectLink.appendChild(projectImg);
+    projectLink.appendChild(projectSpan);
+
+    projectContainer.appendChild(projectLink);
+    return projectContainer;
 }
 
 function onSiteLoaded() {
+    loadProjectSection();
     applySmoothScrolling();
-    loadProjectsToContent();
 }
 
 window.onload = onSiteLoaded;
